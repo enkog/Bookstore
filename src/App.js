e9a6f9c10fdae6feff2bdae7b6a07210036f7539
enkog <nkirukaogu@gmail.com>
2021-09-29 10:01:12 +0100

    Create book form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import NavBar from './components/Layout/NavBar';
 import Home from './pages/Home';
 import Categories from './pages/Categories';
 import Layout from './components/Layout/Layout'
