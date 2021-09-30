87ccc8f637d1dc5cd6911b055e109b245c67d2d2
enkog <nkirukaogu@gmail.com>
2021-09-30 12:07:31 +0100

    Connect React app redux

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
 import './App.css';
-import { v4 as uuidv4 } from 'uuid';
 import Books from './components/Books/Books';
 import Categories from './components/Categories';
 import NavBar from './components/Layout/NavBar';
 
-const DUMMY_BOOKS = [
-  {
-    id: uuidv4(),
-    category: 'Action',
-    title: 'The Hunger games',
-    author: 'Suzanne Collins',
-  },
-  {
-    id: uuidv4(),
-    category: 'Science Fiction',
-    title: 'Dune',
-    author: 'Frank Herbert',
-  },
-  {
-    id: uuidv4(),
-    category: 'Economy',
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-  },
-];
 function App() {
   const [data, setBooks] = useState(DUMMY_BOOKS);
   const addBookHandler = (book) => {
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [
+  {
+    id: uuidv4(),
+    category: 'Action',
+    title: 'The Hunger games',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: uuidv4(),
+    category: 'Science Fiction',
+    title: 'Dune',
+    author: 'Frank Herbert',
+  },
+  {
+    id: uuidv4(),
+    category: 'Economy',
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+  },
+];
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
